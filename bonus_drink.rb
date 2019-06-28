2a492590cc7ec59deecdf57f27b2f311156778b3
Suguru Doki <su-doki@CPFG-PC.local>
2019-06-28 16:53:48 +0900

    first commit

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,15 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    buy = amount
+    drink = 0
+    while buy > 0  do
+      buy = buy - 1
+      drink = drink + 1
+      if drink % 3 == 0 then
+        drink = drink + 1
+      end
+    end
+
+    return drink
   end
 end
